f809bc83d19ab4edb7d2ced2fdb89e5baedad77a
cytoplasma <jpiromg@gmail.com>
2023-01-31 09:48:38 -0800

    finished dashboard-routes, home-routes, auth.js, helpers.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -42,7 +42,8 @@ router.get('/', async (req, res) => {
         const postData = await Post.findAll({
             include: [User],
         });
-        res.render('all-posts', { posts: postData });
+        const posts = postData.map((post) => post.get ({ plain: true}));
+        res.render('all-posts', { posts });
     } catch (err) {
         res.status(500).json(err);
     }
